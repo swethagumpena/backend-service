f3b867c1b4aab074a3311ccb6ef09f9e68aa3c0e
anjali0508 <46600043+anjali0508@users.noreply.github.com>
2021-03-22 12:54:30 +0530

    refactor: remove double quotes"

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-/* eslint-disable quotes */
-const express = require("express");
-const dotenv = require("dotenv");
-const { healthRouter, testRouter } = require("./src/routes");
+const express = require('express');
+const dotenv = require('dotenv');
+const { healthRouter, testRouter } = require('./src/routes');
 
 dotenv.config();
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use("/health", healthRouter);
-app.use("/test", testRouter);
+app.use('/health', healthRouter);
+app.use('/test', testRouter);
 
 app.listen(3000, () => {
   console.log(`Server is up at ${port}`);
