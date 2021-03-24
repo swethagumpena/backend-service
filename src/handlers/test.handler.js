bc8876e4ef1ecfe1ebdf683035a4f8a877fb058b
anjali0508 <46600043+anjali0508@users.noreply.github.com>
2021-03-24 10:15:06 +0530

    refactor: remove DS_Store

--- src/handlers/test.handler.js
+++ src/handlers/test.handler.js
@@ -4,7 +4,8 @@ const testHandler = async (req, res) => {
   const token = req.headers.authorization.split(' ')[1];
   try {
     const loginUser = await authService.verifyToken(token);
-    res.status(201).send(loginUser);
+    console.log(loginUser);
+    res.status(200).send(loginUser);
   } catch (error) {
     res.status(500).send();
   }
